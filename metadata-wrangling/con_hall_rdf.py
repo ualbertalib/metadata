eca39e759f86696c6a29be9811ecb0e237929ba9
Zach Schoenberger <zschoenb@users.noreply.github.com>
2016-06-07 18:52:42 -0600

    removed "id_lib" function

--- metadata-wrangling/con_hall_rdf.py
+++ metadata-wrangling/con_hall_rdf.py
@@ -61,10 +61,6 @@ def create_triples():
     # musicontology.org namespace assigned to "mo"
     mo = Namespace('http://purl.org/ontology/mo/') 
 
-    # establish a library of resources that have already been declared in the graph
-    # so duplicate resources are not created
-    id_lib = { 'CompositionNum': [], 'ComposerURL': [], 'PerformerNum': [], 'PerformanceNum': [], 'RecitalNum': [], 'PerformanceRole': [], 'InstrumentURL': [] }
-
     # the main data source with reconciled and normalized data
     with open('music.csv') as music_file:
         records = csv.DictReader(music_file)
@@ -72,107 +68,99 @@ def create_triples():
     # process one line of the combined csv at a time
         for record in records:       
             # process each entity in the entity list
-            for entity in id_lib.keys():
-                
+            for entity in record.keys():
                 cell_value = record[entity]
+                #determine what column this is
+                if  entity == 'CompositionNum':
+                    # create a composition resource
+                    composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (cell_value) )
+                    # define the resource as a MusicOntology MusicalWork
+                    g.add( (composition, RDF.type, mo.MusicalWork) )
+                    # label the resource with the title of the composition (a string literal)
+                    g.add( (composition, RDFS.label, Literal(str(record['CompositionTitle']))) )
                
-                # check if this entity has been processed already and, if not, add it to the graph
-                # then add it to the list of already processed entities
-                 
-                if cell_value not in id_lib[entity]:
-                    
-                    id_lib[entity].append(cell_value)        
-                    
-                    if  entity == 'CompositionNum':
-                        # create a composition resource
-                        composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (cell_value) )
-                        # define the resource as a MusicOntology MusicalWork
-                        g.add( (composition, RDF.type, mo.MusicalWork) )
-                        # label the resource with the title of the composition (a string literal)
-                        g.add( (composition, RDFS.label, Literal(str(record['CompositionTitle']))) )
-                   
-                    if (entity == 'ComposerURL') & (cell_value != ''):
-                        # create a composer resource
-                        composer = URIRef( cell_value )
-                        # call the compositon resource for linking purposes
-                        composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
-                        # define the resource as a MusicOntology MusicArtist
-                        g.add( (composer, RDF.type, mo.MusicArtist) )
-                        # give the resource a FOAF name (a string literal)
-                        g.add( (composer, FOAF.name, Literal(str(record['ComposerName']))) )
-                        # composition is created by composer
-                        g.add( (composition, DCTERMS.creator, composer) )
-                    
-                    if (entity == 'ComposerNum') & (cell_value != ''):
-                        # create a composer resource
-                        composer = URIRef( 'http://library.ualberta.ca/con_hall/Composer/%s' % (cell_value) )
-                        # call the compositon resource for linking purposes
-                        composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
-                        # define the resource as a MusicOntology MusicArtist
-                        g.add( (composer, RDF.type, mo.MusicArtist) )
-                        # give the resource a FOAF name (a string literal)
-                        g.add( (composer, FOAF.name, Literal(str(record['ComposerName']))) )
-                        # composition is created by composer
-                        g.add( (composition, DCTERMS.creator, composer) )
-                                     
-                    # recital (i.e. recording) resource and properties
-                    if entity == 'RecitalNum':
-                        recording = URIRef( "http://library.ualberta.ca/con_hall/Recording/%s" % (cell_value) )
-                        g.add( (recording, RDF.type, mo.Recording) )
-                        g.add( (recording, RDFS.label, Literal(str(record['RecitalName']))) )
-                        g.add( (recording, DCTERMS.subject, Literal(str(record['RecitalType']))) )
-                        g.add( (recording, DCTERMS.subject, Literal('Convocation Hall')) )
-                        g.add( (recording, DCTERMS.date, Literal(str(record['DateRecorded']))) )                        
+                if (entity == 'ComposerURL') & (cell_value != ''):
+                    # create a composer resource
+                    composer = URIRef( cell_value )
+                    # call the compositon resource for linking purposes
+                    composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
+                    # define the resource as a MusicOntology MusicArtist
+                    g.add( (composer, RDF.type, mo.MusicArtist) )
+                    # give the resource a FOAF name (a string literal)
+                    g.add( (composer, FOAF.name, Literal(str(record['ComposerName']))) )
+                    # composition is created by composer
+                    g.add( (composition, DCTERMS.creator, composer) )
+                
+                if (entity == 'ComposerNum') & (cell_value != ''):
+                    # create a composer resource
+                    composer = URIRef( 'http://library.ualberta.ca/con_hall/Composer/%s' % (cell_value) )
+                    # call the compositon resource for linking purposes
+                    composition = URIRef( "http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
+                    # define the resource as a MusicOntology MusicArtist
+                    g.add( (composer, RDF.type, mo.MusicArtist) )
+                    # give the resource a FOAF name (a string literal)
+                    g.add( (composer, FOAF.name, Literal(str(record['ComposerName']))) )
+                    # composition is created by composer
+                    g.add( (composition, DCTERMS.creator, composer) )
+                                 
+                # recital (i.e. recording) resource and properties
+                if entity == 'RecitalNum':
+                    recording = URIRef( "http://library.ualberta.ca/con_hall/Recording/%s" % (cell_value) )
+                    g.add( (recording, RDF.type, mo.Recording) )
+                    g.add( (recording, RDFS.label, Literal(str(record['RecitalName']))) )
+                    g.add( (recording, DCTERMS.subject, Literal(str(record['RecitalType']))) )
+                    g.add( (recording, DCTERMS.subject, Literal('Convocation Hall')) )
+                    g.add( (recording, DCTERMS.date, Literal(str(record['DateRecorded']))) )                        
 
-                    # performer (i.e. recording) resource and properties
-                    if entity == 'PerformerNum':
-                        performer = URIRef( "http://library.ualberta.ca/con_hall/Performer/%s" % (cell_value) )
-                        g.add( (performer, RDF.type, mo.MusicArtist) )
-                        g.add( (performer, FOAF.name, Literal(str(record['PerformerName']))) )
-                    
-                    # performance (i.e. recording) resource and properties
-                    if entity == 'PerformanceNum':
-                        performance = URIRef( "http://library.ualberta.ca/con_hall/Performance/%s" % (cell_value) )
-                        composition = URIRef("http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
-                        recording = URIRef("http://library.ualberta.ca/con_hall/Recording/%s" % (record['RecitalNum']) )
-                        performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
-                        g.add( (performance, RDF.type, mo.Performance) )
-                        # performance performs composition
-                        g.add( (performance, mo.performance_of, composition ) )
-                        # performance is a part of a recording
-                        g.add( (performance, DCTERMS.isPartOf, recording) )
-                        # performance has performer
-                        g.add( (performer, DCTERMS.isPartOf, performance) )
+                # performer (i.e. recording) resource and properties
+                if entity == 'PerformerNum':
+                    performer = URIRef( "http://library.ualberta.ca/con_hall/Performer/%s" % (cell_value) )
+                    g.add( (performer, RDF.type, mo.MusicArtist) )
+                    g.add( (performer, FOAF.name, Literal(str(record['PerformerName']))) )
+                
+                # performance (i.e. recording) resource and properties
+                if entity == 'PerformanceNum':
+                    performance = URIRef( "http://library.ualberta.ca/con_hall/Performance/%s" % (cell_value) )
+                    composition = URIRef("http://library.ualberta.ca/con_hall/Composition/%s" % (record['CompositionNum']) )
+                    recording = URIRef("http://library.ualberta.ca/con_hall/Recording/%s" % (record['RecitalNum']) )
+                    performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
+                    g.add( (performance, RDF.type, mo.Performance) )
+                    # performance performs composition
+                    g.add( (performance, mo.performance_of, composition ) )
+                    # performance is a part of a recording
+                    g.add( (performance, DCTERMS.isPartOf, recording) )
+                    # performance has performer
+                    g.add( (performer, DCTERMS.isPartOf, performance) )
 
-                    # performer role (linked data instance)
-                    if (entity == 'InstrumentURL')  & (cell_value != ''):
-                        performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
-                        performance = URIRef("http://library.ualberta.ca/con_hall/Performance/%s" % (record['PerformanceNum']) )
-                        performanceRole = URIRef( str(record[entity]) )
+                # performer role (linked data instance)
+                if (entity == 'InstrumentURL')  & (cell_value != ''):
+                    performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
+                    performance = URIRef("http://library.ualberta.ca/con_hall/Performance/%s" % (record['PerformanceNum']) )
+                    performanceRole = URIRef( str(record[entity]) )
+                    g.add( (performanceRole, RDF.type, mo.Instrument) )
+                    g.add( (performanceRole, RDFS.label, Literal(str(record['PerformanceLabel']))) )
+                    # performer role is attributed both to the performance and to the performer
+                    # the role the performer played in a given performance needs to be inferred
+                    # a performer's role can potentially be ambiguous
+                    g.add( (performance, mo.instrument, performanceRole))
+                    g.add( (performer, mo.instrument, performanceRole))
+                
+                # performer role (literal instance)
+                if (entity == 'PerformanceRole') & (cell_value != ''):
+                    cell_value = cell_value.replace(' ','_')
+                    cell_value = cell_value.replace(',','')
+                    performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
+                    performance = URIRef("http://library.ualberta.ca/con_hall/Performance/%s" % (record['PerformanceNum']) )
+                    performanceRole = URIRef( "http://library.ualberta.ca/con_hall/Role/%s" % (cell_value) )
+                    g.add( (performanceRole, RDFS.label, Literal(str(record['PerformanceLabel']))) )
+                    # if the performer role is conductor, this is typed
+                    if cell_value == 'conductor':
+                        g.add( (performance, mo.conductor, performanceRole) )
+                        g.add( (performer, mo.conductor, performanceRole) )
+                    else:
                         g.add( (performanceRole, RDF.type, mo.Instrument) )
-                        g.add( (performanceRole, RDFS.label, Literal(str(record['PerformanceLabel']))) )
-                        # performer role is attributed both to the performance and to the performer
-                        # the role the performer played in a given performance needs to be inferred
-                        # a performer's role can potentially be ambiguous
-                        g.add( (performance, mo.instrument, performanceRole))
-                        g.add( (performer, mo.instrument, performanceRole))
-                    
-                    # performer role (literal instance)
-                    if (entity == 'PerformanceRole') & (cell_value != ''):
-                        cell_value = cell_value.replace(' ','_')
-                        cell_value = cell_value.replace(',','')
-                        performer = URIRef("http://library.ualberta.ca/con_hall/Performer/%s" % (record['PerformerNum']) )
-                        performance = URIRef("http://library.ualberta.ca/con_hall/Performance/%s" % (record['PerformanceNum']) )
-                        performanceRole = URIRef( "http://library.ualberta.ca/con_hall/Role/%s" % (cell_value) )
-                        g.add( (performanceRole, RDFS.label, Literal(str(record['PerformanceLabel']))) )
-                        # if the performer role is conductor, this is typed
-                        if cell_value == 'conductor':
-                            g.add( (performance, mo.conductor, performanceRole) )
-                            g.add( (performer, mo.conductor, performanceRole) )
-                        else:
-                            g.add( (performanceRole, RDF.type, mo.Instrument) )
-                            g.add( (performance, mo.instrument, performanceRole) )
-                            g.add( (performer, mo.instrument, performanceRole) )
+                        g.add( (performance, mo.instrument, performanceRole) )
+                        g.add( (performer, mo.instrument, performanceRole) )
     
     # generates the xml-rdf serialization and exports to file
     g.serialize(destination='con_hall.rdf', format='pretty-xml')
