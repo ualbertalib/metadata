f202fdba4cd052b95c10f14b5f9f7fc17a62bc75
Zach Schoenberger <zschoenb@users.noreply.github.com>
2016-06-07 18:26:36 -0600

    Revert "added a namespace de-anonymization script"
    This reverts commit 5e2a0e5d500ab7de0f5fafc360a3cffad99d5e16.

--- metadata-wrangling/con_hall_rdf.py
+++ metadata-wrangling/con_hall_rdf.py
@@ -1,5 +1,3 @@
-#!usr/bin/python3
-
 """ 
 a legacy database of "convocation hall" music recordings metadata was converted from MSAccess to a single csv.
 data was normalized (open refine) and reconciled using the musicbrainz vocabulary (see import function).
@@ -14,8 +12,9 @@ from rdflib.namespace import FOAF, RDFS, RDF, DCTERMS
 import re
 from time import sleep
 
+
 def main():
-    # import_from_music_ontology()
+    import_from_music_ontology()    
     create_triples()
 
 
@@ -49,6 +48,7 @@ def import_from_music_ontology():
 
 
 def create_triples():
+    
     """
     generates an in-memory rdf graph of triples describing 
     the University of Alberta Convocation hall recordings
@@ -163,13 +163,9 @@ def create_triples():
                         g.add( (performer, mo.instrument, performanceRole) )
     
     # generates the xml-rdf serialization and exports to file
-    g.serialize(destination='con_hall.rdf', format='pretty-xml')
-    # appends the generic namespaces with specifics
-    with open('con_hall.rdf', 'r') as f:
-        ch = f.read()
-        ch = ch.replace('ns1','mo').replace('ns2','dcterms').replace('ns3','foaf')
-    with open('con_hall.rdf', 'w') as f:
-        f.write(ch)
+    g.serialize(file='con-hall.rdf', format='pretty-xml')
+   
 
-if __name__ == '__main__':
+if __name__ is "__main__":
     main()
+
