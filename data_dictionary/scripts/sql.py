91b5c76107bdbed4d408853c5c06f3117f922614
zschoenb <zschoenb@ualberta.ca>
2017-09-01 16:12:39 -0600

    mysql script

---
+++ data_dictionary/scripts/sql.py
@@ -0,0 +1,41 @@
+import json
+import mysql.connector
+from mysql.connector import Error
+from hydraNorth import hydraNorth
+from secret import config
+
+main():
+	conn = connect()
+	cursor = conn.cursor()
+	createDB(cursor)
+	cursor.close()
+	conn.close()
+
+def createDB(cursor):
+	for profileType in ['collection', 'generic', 'thesis']:
+		filename = '../profiles/%s/profile.json'
+		with open(filename, 'w+') as data:
+			for item in json.load(data):
+				query = "INSERT INTO %s SET uri='%s';" % (profileType, data['item']['uri'])
+				cursor.execute(query)
+				for instance in data['item']['acceptedValues']:
+					query = "INSERT INTO instances VALUES ('%s', '%s')" % (instance['uri'], instance['label'])
+					cursor.execute(query)
+					query = "INSERT INTO instanceMap VALUES('%s', '%s')" % (instance['uri'], data['item']['uri'])
+					cursor.execute(query)
+
+def connect():
+    """ Connect to MySQL database """
+    try:
+        conn = mysql.connector.connect()
+        if conn.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    return conn
+
+
+if __name__ == '__main__':
+    main()
