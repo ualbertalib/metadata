8d03b48401812f6cebfd2089b2c1703caee3f1d8
danydvd <danoosh@ualberta.ca>
2017-11-02 15:11:55 -0600

    DD Google sheets

--- data_dictionary/scripts/lib/__init__.py
+++ data_dictionary/scripts/lib/__init__.py
@@ -6,13 +6,13 @@ import sys
 
 
 def main():
-	'''old_stdout = sys.stdout
+	old_stdout = sys.stdout
 	with open('data_dictionary/jupiter_ontology.md', "w+") as ontology:
 		sys.stdout = ontology
 		owlDocument().generate()
 	sys.stdout = old_stdout
 	for ptype in ["collection", "community", "generic", "thesis"]:
-		Profiler(ptype)'''
+		Profiler(ptype)
 	excelGen()
 	google_generate()
 
