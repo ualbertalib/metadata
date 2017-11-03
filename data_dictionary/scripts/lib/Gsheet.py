567cfb9973771e5c7283198c6e38445173ea81e2
danydvd <danoosh@ualberta.ca>
2017-11-03 12:04:50 -0600

    DD Google

--- data_dictionary/scripts/lib/Gsheet.py
+++ data_dictionary/scripts/lib/Gsheet.py
@@ -20,7 +20,7 @@ except ImportError:
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
-CLIENT_SECRET_FILE = 'client_secret.json'
+CLIENT_SECRET_FILE = 'data_dictionary/scripts/lib/client_secret.json'
 APPLICATION_NAME = 'Google Sheets API Quickstart'
 
 
