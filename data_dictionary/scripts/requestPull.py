b19ddb7537cb0ab209450acde0b82aa33a059a77
Unknown <zschoenb@ualberta.ca>
2017-11-10 15:06:18 -0700

    change auth type

---
+++ data_dictionary/scripts/requestPull.py
@@ -0,0 +1,7 @@
+from secret import password, client_id, client_secret
+
+git = Github('zschoenb',password)
+org = git.get_organization('ualbertalib')
+repo = org.get_repo('metadata')
+pull = repo.create_pull(title="Daily Data Dictionary Update", head="data_dictionary_update", base="master", body="See commit message for yesterdays changes to the data dictionary profiles.")
+repo.issue(pull["number"]).edit(assignees=["zschoenb", "danydvd", "sfarnel", "johnhuck"], lables=['Jpuiter Data Dictionary'])
