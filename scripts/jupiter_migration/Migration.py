1e54049c28d7376f2c9086174315323a333e670b
Unknown <zschoenb@ualberta.ca>
2017-12-04 15:58:43 -0700

    removed some tests

--- scripts/jupiter_migration/Migration.py
+++ scripts/jupiter_migration/Migration.py
@@ -11,9 +11,6 @@ import re
 import json
 import requests
 import random
-import nltk
-#nltk.download('punkt')
-from nltk import word_tokenize
 proxyHash = {}
 fileSetTracker = []
 
@@ -84,10 +81,6 @@ class TransformationFactory():
                 return Transformation().createdDate(subjects, triple, objectType)
             if (subjects['subject'] in triple['subject']['value']) and (function == "graduationdate"):
                 return Transformation().gradDate(subjects, triple, objectType)
-        if function == "created":
-            return Transformation().sortYear(triple, objectType)
-        if function == "graduationdate":
-            return Transformation().sortYear(triple, objectType)
         if function == "accessRights":
             return Transformation().accessRights(triple, objectType)
         elif function == "modelsmemberOf":
@@ -195,7 +188,6 @@ class Data(object):
                     s_o[s].append(o)
             for so in s_o:
                 if URIRef('http://terms.library.ualberta.ca/embargo') in s_o[so]:
-                    print(URIRef(so), 'should be embargo')
                     self.graph.remove((URIRef(so), URIRef("http://purl.org/dc/terms/accessRights"), URIRef('http://terms.library.ualberta.ca/public')))
                     self.graph.remove((URIRef(so), URIRef("http://purl.org/dc/terms/accessRights"), URIRef('http://terms.library.ualberta.ca/authenticated')))
                     self.graph.remove((URIRef(so), URIRef("http://purl.org/dc/terms/accessRights"), URIRef('http://terms.library.ualberta.ca/draft')))
@@ -252,69 +244,6 @@ class Data(object):
         headers = {'Content-Type': 'text/turtle'}
         requests.post(sparqlResults, data=self.graph.serialize(format='nt'), headers=headers)
 
-
-class DateFinder:
-    def __init__(self, tokens):
-        self.word = tokens
-        self.out = []
-        self.output = [""]
-        
-    def getyear(self):
-        # find all years with a 4 digit format (between 1900 and 2018)
-        self.year = [i for i, w in enumerate(self.word) if re.search('^\d{4}$', w)]
-        #print (self.year)
-        for n,year in enumerate(self.year):
-            self.out.append({'year': '', "grams": {"1-gram": "", "-1-gram": "", "2-gram": "", "-2-gram": "", "3-gram": "", "-3-gram": "", "4-gram": "", "-4-gram": "", "5-gram": "", "-5-gram": ""}})
-            if int(self.word[year]) > 1900 and int(self.word[year]) < 2018:
-                #index = self.word.index(year)
-                self.out[n]["year"] = self.word[year]
-                for j in range(1, 6):
-                    # preceding grams
-                    if j + year < len(self.word): 
-                        self.out[n]["grams"][str(j) + "-gram"] = self.word[year + j] 
-                    #succeeding grams 
-                    if year - j > 0:
-                        self.out[n]["grams"]["-" + str(j) + "-gram"] = self.word[year - j]
-                self.output = self.out 
-        if self.output[0] != '':
-            return self.output
-        else: 
-            return None
-    
-    def getymonth(self):
-        # find all dates with yyyy-./:mm format
-        self.ymonth = [w for w in self.word if re.search('^\d{4}?[-/.//://\/]\d{2}$', w)]
-        for i in self.ymonth:
-            for j in ["-", ":", "/", "."]:
-                if j in i:
-                    self.year = ""
-                    self.month = ""
-                    self.year = i.split(str(j))[0]
-                    self.month = i.split(str(j))[1]
-                if self.year in range (1900, 2019) and self.month in range (1, 13):
-                    self.output[0] = self.month + "/" + self.year
-        if self.output[0] != '':
-            return self.output
-        else:
-            return None
-    
-    def getyday(self):
-        self.yday = [w for w in self.word if re.search('^\d{4}?[-/.//://\/]\d{2}?[/.//:/-/\/]\d{2}$', w)]
-        for i in self.yday:
-            for j in ["-", ":", "/", "."]:
-                if j in i:
-                    self.year = ""
-                    self.month = ""
-                    self.day = ""
-                    self.year = i.split(str(j))[0]
-                    self.month = i.split(str(j))[1]
-                    self.day = i.split(str(j))[2]
-                if self.year in range (1900, 2019) and self.month in range (1, 13) and slef.day in range (1, 32):
-                    self.output[0] = self.day + "/" + self.month + "/" + self.year
-        if self.output[0] != '':
-            return self.output
-        else:
-            return None  
 
 class Query(object):
     """ Query objects are dynamically generated, and contain SPARQL CONSTRUCT queries with input from the jupiter application profile """
@@ -1013,7 +942,6 @@ class Transformation():
 
     def available(self, triple, objectType):
         if datetime.strptime(re.sub(r"[T].+$", "", triple['object']['value']), "%Y-%m-%d").date() > date.today():
-            print(triple['subject']['value'])
             self.output.append(
             {
                 'subject': {
@@ -1032,46 +960,8 @@ class Transformation():
             )
         self.output.append(triple)
         return self.output
-        
+
     def createdDate(self, subjects, triple, objectType):
-        tempTriple = {
-            'subject': {
-                'value': triple['subject']['value'],
-                'type': 'uri'
-            },
-            'predicate': {
-                'value': triple['predicate']['value'],
-                'type': 'uri'
-            },
-            'object': {
-                'value': subjects["object"][0],
-                'type': 'date'
-            }
-        }
-        self.output.append(tempTriple)
-        Transformation.sortYear(self, tempTriple, objectType)
-        return self.output
-
-    def gradDate(self, subjects, triple, objectType):
-        tempTriple = {
-            'subject': {
-                'value': triple['subject']['value'],
-                'type': 'uri'
-            },
-            'predicate': {
-                'value': triple['predicate']['value'],
-                'type': 'uri'
-            },
-            'object': {
-                'value': subjects["object"][0],
-                'type': 'date'
-            }
-        }
-        self.output.append(tempTriple)
-        Transformation.sortYear(self, tempTriple, objectType)
-        return self.output
-
-    def sortYear(self, triple, objectType):
         self.output.append(
             {
                 'subject': {
@@ -1083,44 +973,32 @@ class Transformation():
                     'type': 'uri'
                 },
                 'object': {
-                    'value': triple['object']['value'],
+                    'value': subjects["object"],
                     'type': 'date'
                 }
             }
         )
-        if isinstance(triple['object']['value'], list):
-            text = triple['object']['value'][0].replace(';', ' ').replace(':', ' ').replace('_', ' ').replace('-', ' ').replace('/', ' ').replace('.', ' ').replace(',', ' ')
-        else:
-            text = triple['object']['value'].replace(';', ' ').replace(':', ' ').replace('_', ' ').replace('-', ' ').replace('/', ' ').replace('.', ' ').replace(',', ' ')
-        tokens = word_tokenize(text)
-        for n,i in enumerate(tokens):
-            if i == "," :
-                del tokens[n]
-            if i == ")" :
-                del tokens[n]
-            if i == "(" :
-                del tokens[n]
-        years = DateFinder(tokens)
-        trans = years.getyear()
-        if trans != None:
-            for i in trans:
-                self.output.append(
-                    {
-                        'subject': {
-                            'value': triple['subject']['value'],
-                            'type': 'uri'
-                        },
-                        'predicate': {
-                            'value': 'http://terms.library.ualberta.ca/date/sortyear',
-                            'type': 'uri'
-                        },
-                        'object': {
-                            'value': i["year"],
-                            'type': 'date'
-                        }
-                    }
-                )
-            return self.output
+        return self.output
+
+    def gradDate(self, subjects, triple, objectType):
+        print('gradDate', triple['subject']['value'])
+        self.output.append(
+            {
+                'subject': {
+                    'value': triple['subject']['value'],
+                    'type': 'uri'
+                },
+                'predicate': {
+                    'value': triple['predicate']['value'],
+                    'type': 'uri'
+                },
+                'object': {
+                    'value': subjects["object"],
+                    'type': 'date'
+                }
+            }
+        )
+        return self.output
 
     def owner(self, triple, objectType):
         triple['object']['value'] = triple['subject']['value'].strip("http://projecthydra.org/ns/auth/person#")
