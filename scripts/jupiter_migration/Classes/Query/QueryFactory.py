89fb149f718249dfe8715fdf9b56d15b40f3729b
Unknown <zschoenb@ualberta.ca>
2017-12-11 10:35:43 -0700

    separated querybuilder

--- scripts/jupiter_migration/Classes/Query/QueryFactory.py
+++ scripts/jupiter_migration/Classes/Query/QueryFactory.py
@@ -1,20 +1,20 @@
-from Classes import Query
+from Classes.Query.QueryBuilder import *
 
 class queryFactory():
     @staticmethod
-    def getMigrationQuery(queryHelper):
+    def getMigrationQuery(objectType, tripleStoreData, uri_generator):
         """ returns a specified query object depending on the type passed in"""
-        if queryHelper.objectType == "collection":
-            return Query.Collection(queryHelper)
-        elif queryHelper.objectType == "community":
-            return Query.Community(queryHelper)
-        elif queryHelper.objectType == "thesis":
-            return Query.Thesis(queryHelper)
-        elif queryHelper.objectType == "generic":
-            return Query.Generic(queryHelper)
-        elif queryHelper.objectType == "technical":
-            return Query.Technical(queryHelper)
-        elif queryHelper.objectType == "relatedObject":
-            return Query.Related_Object(queryHelper)
+        if objectType == "collection":
+            return Collection(objectType, tripleStoreData, uri_generator)
+        elif objectType == "community":
+            return Community(objectType, tripleStoreData, uri_generator)
+        elif objectType == "thesis":
+            return Thesis(objectType, tripleStoreData, uri_generator)
+        elif objectType == "generic":
+            return Generic(objectType, tripleStoreData, uri_generator)
+        elif objectType == "technical":
+            return Technical(objectType, tripleStoreData, uri_generator)
+        elif objectType == "relatedObject":
+            return Related_Object(objectType, tripleStoreData, uri_generator)
         else:
             return None
