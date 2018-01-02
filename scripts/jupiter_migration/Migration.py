f84382a896d1a97fbba927be295fdf170991b135
Unknown <zschoenb@ualberta.ca>
2018-01-02 16:36:48 -0700

    more comments

--- scripts/jupiter_migration/Migration.py
+++ scripts/jupiter_migration/Migration.py
@@ -12,24 +12,21 @@ from datetime import datetime
 def main():
     """ main controller: iterates over each object type (generic item metadata, thesis item metadata, and binary-level metadata),
     creates a set of subqueries for each of these types, then cues threads to run each of these subqueries as a job. The migration outout is saved to
-    the results folder and to a triplestore. The subqueries are cached in the cache folder. Custom settings can be modified in config.py."""
-    ts = datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')
-    tripleStoreData = Triple_Store.TripleStore(sparqlData, sparqlTerms, sparqlResults)
-    uri_generator = URI_Generator.URIGenerator()
-    cleanOutputs(sparqlResults)
+    the results folder. The subqueries are cached in the cache folder. Custom settings can be modified in config.py."""
+    ts = datetime.fromtimestamp(time.time()).strftime('%H:%M:%S') # a timestamp for observing the script walltime
+    tripleStoreData = Triple_Store.TripleStore(sparqlData, sparqlTerms, sparqlResults) # sets all of the endpoints on one object
+    uri_generator = URI_Generator.URIGenerator() # an object for handling the creation and management of proxies
+    cleanOutputs(sparqlResults) # erase the contents of the query cache and the results folder
     # Iterate over every type of object that needs to be migrated.
-    # This is the first splitting of the data for migration.
-    # a queryObject has been split into multiple groups
-    # only one group exists for community, and one for collection objects
-    # approximately a thousand queries each are minted for thesis and for generic objects
-    # these queries are based on the first folder in the fedora pair tree
-    # a queryObject knows its type
-    # a cache of queries is recorded, results are sent to a "results" triplestore, and results are stored as n-triples
     for objectType in types:
+        # the queryFactory getMigrationQuery method returns a query object depending on the type it was passed
+        # a query object contains all of the queries needed to obtain the data for this type, split into manageably sized groups
         queryObject = Query_Factory.QueryFactory().getMigrationQuery(objectType, tripleStoreData)
+        # tell us a little about this query object
         print('{0} queries generated'.format(objectType))
         print('{0} queries of {1} objects to be transformed'.format(len(queryObject.queries), objectType))
         i = 0
+        # spawn 8 workers, one for each group in the query object. A data object (see parellelTransform) is created for each group. When the 8 are finished, they return for more queries.
         with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
             future_to_result = {executor.submit(parellelTransform, group, queryObject, uri_generator): group for group in queryObject.queries.keys()}
             for future in concurrent.futures.as_completed(future_to_result):
@@ -47,6 +44,7 @@ def main():
 
 
 def parellelTransform(group, queryObject, UG):
+    """the query object, along with a group, along with the uri generator object, are passed into a data object, where transformations occur and the data is saved"""
     DTO = Data.Data(group, queryObject)  # query, group, object
     DTO.transformData(UG)
     # DTO.resultsToTriplestore()
