ef1c41d189cd48df695ee2c05771611037fe113a
danydvd <danoosh@ualberta.ca>
2018-01-23 12:12:02 -0700

    housekeeping

--- data_dictionary/scripts/lib/excelg.py
+++ data_dictionary/scripts/lib/excelg.py
@@ -5,12 +5,12 @@ from openpyxl import Workbook
 
 
 def excelGen():
-    os.remove('data_dictionary/profiles.xlsx')
+    os.remove('../..//profiles.xlsx')
     #../../profiles.xlsx
     wb = Workbook()
     ws = wb.active
     ws['A1'] = ''
-    wb.save('data_dictionary/profiles.xlsx')
+    wb.save('../../profiles.xlsx')
     #../../profiles.xlsx
     for profile in ['community', 'collection', 'generic', 'thesis', 'oai_pmh', 'oai_etdms']:
     	Excel(profile)
