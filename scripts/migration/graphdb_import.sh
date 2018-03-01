3bb03e34d824fa9802e8ee3054c7eb3fe074c9cb
anayram <anayram@gmail.com>
2018-03-01 15:16:55 -0700

    test

--- scripts/migration/graphdb_import.sh
+++ scripts/migration/graphdb_import.sh
@@ -3,11 +3,11 @@ var = 0
 #'community' 'collection'
 for folder in 'generic' #'technical' 'relatedObject' #'thesis'
 do
-	for file in $(ls -p ~/metadata_work/jupiter_migration/results/$folder/)
+	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results-feb-23-generic/$folder/)
 		do
 			var=$((var+1))
 			echo $var
 			echo $folder/$file 
-			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file http://206.167.181.124:7200/repositories/migration_2018-01/statements
+			curl -X POST -H "Content-Type:application/n-triples" -T results-feb-23-generic/$folder/$file http://206.167.181.124:7200/repositories/generic-feb-23/statements
 		done
 done
