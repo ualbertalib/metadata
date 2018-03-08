d393967a743703f1cc690a886c689e50328f3499
danydvd <danoosh@ualberta.ca>
2018-03-07 17:12:26 -0700

    adding more dates

--- scripts/migration/graphdb_import.sh
+++ scripts/migration/graphdb_import.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 var = 0
 #'community' 'collection'
-for folder in 'generic' #'technical' 'relatedObject' #'thesis'
+for folder in 'community' 'collection' 'generic' 'thesis' #'technical' 'relatedObject'
 do
-	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results-feb-23-generic/$folder/)
+	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results/$folder/)
 		do
 			var=$((var+1))
 			echo $var
 			echo $folder/$file 
-			curl -X POST -H "Content-Type:application/n-triples" -T results-feb-23-generic/$folder/$file http://206.167.181.124:7200/repositories/generic-feb-23/statements
+			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file http://206.167.181.124:7200/repositories/migration_20180222/statements
 		done
 done
