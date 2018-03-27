0a6893f2adad2af2f196171dbb762319df153bb4
danydvd <danoosh@ualberta.ca>
2018-03-27 09:25:26 -0600

    fixing date isssues

--- scripts/migration/graphdb_import.sh
+++ scripts/migration/graphdb_import.sh
@@ -2,13 +2,13 @@
 python Migration.py
 var = 0
 
-for folder in 'community' 'collection' 'generic' 'thesis' #'technical' 'relatedObject'
+for folder in "generic" #'community' 'collection' 'generic' 'thesis' #'technical' 'relatedObject'
 do
 	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results/$folder/)
 		do
 			var=$((var+1))
 			echo $var
 			echo $folder/$file 
-			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file http://206.167.181.124:7200/repositories/migration_mar_14/statements
+			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file http://206.167.181.124:7200/repositories/mar_26_generic/statements
 		done
 done
