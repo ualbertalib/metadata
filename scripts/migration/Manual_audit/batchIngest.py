72148ba296a49a48bb2c7644929545c40b984f46
danydvd <danoosh@ualberta.ca>
2018-03-28 17:09:41 -0600

    batch ingested problematic items

---
+++ scripts/migration/Manual_audit/batchIngest.py
@@ -0,0 +1,22 @@
+from SPARQLWrapper import JSON, SPARQLWrapper
+import json
+
+with open("list.txt", "r") as file:
+	l = []
+	for line in file:
+		if line.replace("\n", "") not in l:
+			l.append(line.replace("\n", ""))
+print (len(l))
+sparqlData = "http://localhost:9999/blazegraph/namespace/gillingham_20180222/sparql"
+sparql = SPARQLWrapper(sparqlData)
+sparql.setReturnFormat(JSON)
+f = []
+with open("batch_ingested.tsv", "w+") as o:
+	for i in l:
+		query = "select ?batch where { <%s> <http://terms.library.library.ca/identifiers/ingestbatch> ?batch . filter(?batch != '') }" %(i) # http://terms.library.library.ca/id/ingestbatch does not exists
+		sparql.setQuery(query)
+		results = sparql.query().convert()['results']['bindings']
+		for r in results:
+			o.write(i + "\t" + r['batch']['value'] + "\n")
+
+			
