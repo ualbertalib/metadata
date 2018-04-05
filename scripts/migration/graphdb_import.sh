74c307ae860b897c5fab247faf3442f29ab68338
danydvd <danoosh@ualberta.ca>
2018-04-05 16:06:23 -0600

    bringing remote up-to-date

--- scripts/migration/graphdb_import.sh
+++ scripts/migration/graphdb_import.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-#python Migration.py
+python Migration.py
 var = 0
 
-for folder in "generic" #'community' 'collection' 'generic' 'thesis' #'technical' 'relatedObject'
+for folder in 'community' 'collection' 'generic' 'thesis' #'technical' 'relatedObject'
 do
-	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results/$folder/)
+	for file in $(ls -p ~/git/remote/metadata/scripts/migration/results/inJupiter/$folder/)
 		do
 			var=$((var+1))
 			echo $var
 			echo $folder/$file 
-			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file http://206.167.181.124:7200/repositories/mar_27_generic/statements
+			curl -X POST -H "Content-Type:application/n-triples" -T results/inJupiter/$folder/$file http://206.167.181.124:7200/repositories/thesis-apr-5/statements
+			#curl -X POST -H "Content-Type:application/n-triples" -T results/inJupiter/$folder/$file http://206.167.181.124:7200/repositories/migration_mar_29/statements
+		done
+	for file1 in $(ls -p ~/git/remote/metadata/scripts/migration/results/$folder/)
+		do
+			var=$((var+1))
+			echo $var
+			echo $folder/$file1 
+			curl -X POST -H "Content-Type:application/n-triples" -T results/$folder/$file1 http://206.167.181.124:7200/repositories/thesis-apr-5/statements
 		done
 done
