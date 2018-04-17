ae25c07480c4a25169314ad383842000c8c5ae48
danydvd <danoosh@ualberta.ca>
2018-04-17 10:09:47 -0600

    updating migration package from fedora

---
+++ scripts/migration/jupiter_audit/getPremsFedora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+
+	name="$(cut -d'/' -f11 <<<"$line")"
+	echo $name
+	curl -u "fedoraAdmin:HN4di2015" -H "Content-type:application/xml" -X GET $line >> "CPermissions.nt"
+done < "cperms-uri.txt"
