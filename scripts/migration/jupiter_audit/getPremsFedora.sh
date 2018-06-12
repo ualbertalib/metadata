2851c5a2689a86dbbea3a27879eaa804d13797c4
danydvd <danoosh@ualberta.ca>
2018-06-12 15:23:51 -0600

    adding removed files

---
+++ scripts/migration/jupiter_audit/getPremsFedora.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while IFS='' read -r line || [[ -n "$line" ]]; do
+    echo "Text read from file: $line"
+
+	name="$(cut -d'/' -f11 <<<"$line")"
+	echo $name
+	curl -u "username:password" -H "Content-type:application/xml" -X GET $line >> "CPermissions.nt"
+done < "cperms-uri.txt"
