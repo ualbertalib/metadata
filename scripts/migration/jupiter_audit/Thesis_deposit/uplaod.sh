b9683b26c9882fc7077d70d166a2e26367edfda2
danydvd <danoosh@ualberta.ca>
2018-06-14 09:40:50 -0600

    Rewriting Thesis Deposit items

--- scripts/migration/jupiter_audit/Thesis_deposit/uplaod.sh
+++ scripts/migration/jupiter_audit/Thesis_deposit/uplaod.sh
@@ -2,5 +2,5 @@ FILES="/home/danydvd/git/remote/metadata/scripts/migration/jupiter_audit/Thesis_
 for f in $FILES
 do
 	echo $f
-	curl -H 'Content-Type: text/turtle' --upload-file $f -X POST "http://206.167.181.124:9999/blazegraph/namespace/Thesis_deposit_Jun112018/sparql"
+	curl -H 'Content-Type: text/turtle' --upload-file $f -X POST "http://206.167.181.124:9999/blazegraph/namespace/Thesis_deposit_Jun132018/sparql"
 done
