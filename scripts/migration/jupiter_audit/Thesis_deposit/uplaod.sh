1cd5b671a6f7c55d723c69488e56da8c33649051
danydvd <danoosh@ualberta.ca>
2018-07-18 10:07:31 -0600

    generating migration package for THesis-deposit remaining items

--- scripts/migration/jupiter_audit/Thesis_deposit/uplaod.sh
+++ scripts/migration/jupiter_audit/Thesis_deposit/uplaod.sh
@@ -1,6 +1,6 @@
-FILES="/home/danydvd/git/remote/metadata/scripts/migration/jupiter_audit/Thesis_deposit/perms/*"
+FILES="/home/danydvd/git/remote/metadata/scripts/migration/jupiter_audit/Thesis_deposit/embargo-jul132108/*"
 for f in $FILES
 do
 	echo $f
-	curl -H 'Content-Type: text/turtle' --upload-file $f -X POST "http://206.167.181.124:9999/blazegraph/namespace/Thesis_deposit_Jun132018/sparql"
+	curl -H 'Content-Type: text/turtle' --upload-file $f -X POST "http://206.167.181.124:9999/blazegraph/namespace/Thesis_deposit_Jul132018/sparql"
 done
