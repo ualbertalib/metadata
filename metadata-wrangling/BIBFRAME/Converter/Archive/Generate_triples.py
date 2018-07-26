3552c898bd00d626c8b0de719ea6df0550c8141f
danydvd <danoosh@ualberta.ca>
2018-07-26 11:59:47 -0600

    cleaning results and Archive folder from github

--- metadata-wrangling/BIBFRAME/Converter/Archive/Generate_triples.py
+++
@@ -1,8 +0,0 @@
-import rdflib
-
-g = rdflib.Graph()
-result = g.parse("UADATA-Seg-2-enhanced-test.xml")
-
-print("graph has %s statements." % len(g))
-
-s = g.serialize(destination='test.nt', format='nt')
