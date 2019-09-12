a6eca48dffa79173be932ff176ac1f4295b5518d
danydvd <danoosh@ualberta.ca>
2019-09-12 11:43:41 -0600

    adding MODS metadata

--- scripts/ia/Peel_to_IA/test.py
+++ scripts/ia/Peel_to_IA/test.py
@@ -4,6 +4,7 @@ from Process_factory import *
 from Util import *
 
 with open('upload-log.txt', 'a') as log:
+	code = 'VCA'
 	for index, dir in enumerate(os.listdir('Vulcan')):
 		try:
 			if index < 2500:
@@ -11,15 +12,16 @@ with open('upload-log.txt', 'a') as log:
 				path = 'Vulcan/%s' %(dir)
 				print ('%s strat processing %s' %(index+1, path))
 				make_process_folder(path)
-				metadata_obj = metadata(path)
+				metadata_obj = metadata(path, code)
 				metadata_obj.untar_mets_alto()
-				metadata_process = metadata_obj.get_mets_data()
+				top_mods = metadata_obj.get_mods()
+				metadata_process = metadata_obj.get_mets_data(top_mods)
 				item_id = metadata_process[0]
 				meta = metadata_process[1]
 				image_obj = images(path, item_id)
 				image = image_obj.generate_tar()
-				uplaod_obj = file_uplaod(path, item_id, meta, log)
-				uplaod = uplaod_obj.upload_to_IA()
+				#uplaod_obj = file_uplaod(path, item_id, meta, log)
+				#uplaod = uplaod_obj.upload_to_IA()
 				print ('----------------------------------------------------------')
 			else:
 				break
