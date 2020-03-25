76e662f8dbf5723b4862f0112299e1cd0ccb97a1
danydvd <danoosh@ualberta.ca>
2020-03-24 22:08:11 -0600

    technical_triples work

--- scripts/Technical_triples/t.py
+++ scripts/Technical_triples/t.py
@@ -2,6 +2,7 @@ import requests
 from Passwords import passwords
 import re
 from SPARQLWrapper import JSON, SPARQLWrapper, RDFXML, N3
+from time import sleep
 
 passwd = passwords['gillingham2'][1]
 user = passwords['gillingham2'][0]
@@ -17,14 +18,34 @@ PREFIX bibo: <http://purl.org/ontology/bibo/>
 PREFIX dcterms: <http://purl.org/dc/terms/>
 select distinct ?s ?o where { 
 	?s <http://fedora.info/definitions/v4/repository#hasFixityService> ?o.
-} limit 200"""
+} limit 500000"""
 sparqlData.setQuery(query)  # set the query
 results = sparqlData.query().convert()
 for i, result in enumerate(results['results']['bindings']):
+	print ("processing %s of %s" %(i+1, len(results['results']['bindings'])))
 	fixity = None
 	subject = result['s']['value'].replace('mycombe', 'gillingham2')
 	fixity = result['o']['value'].replace('mycombe', 'gillingham2')
-	response = requests.get(fixity, headers=headers, auth=(user, passwd)).text
-	matched = re.search("#fixity/\S+", response)
-	if matched:
-		print (response.replace(matched.group(), '#fixity>'))
+	fresponse = requests.get(fixity, headers=headers, auth=(user, passwd)).text
+	mresponse = requests.get(fixity.replace('fixity', 'metadata'), headers=headers, auth=(user, passwd)).text
+	fmatched = re.search("#fixity/\S+", fresponse)
+	if fmatched:
+		print ("writing %s fixity" %(subject.split('/')[-1]))
+		with open('fixity/%s.ttl' %(subject.split('/')[-1]), 'w') as file:
+			file.write(fresponse.replace(fmatched.group(), '/fcr:fixity>').replace('premis:hasFixity', 'fedora:hasFixityService').replace('gillingham2', 'mycombe'))
+			file.close()
+	else:
+		with open('fixity/fixity.log', 'a+') as fixlog:
+			fixlog.write("no fixity reponse for %s\n" %(subject.split('/')[-1]))
+			fixlog.close()
+	if mresponse != '':
+		print ("writing %s metadata" %(subject.split('/')[-1]))
+		with open('metadata/%s.ttl' %(subject.split('/')[-1]), 'w') as mfile:
+			mfile.write(mresponse.replace('gillingham2', 'mycombe'))
+			mfile.close()
+	else:
+		with open('metadata/metadata.log', 'a+') as mlog:
+			mlog.write("no metadata reponse for %s\n" %(subject.split('/')[-1]))
+			mlog.close()
+	sleep(1)
+
